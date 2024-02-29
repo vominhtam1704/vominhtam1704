6e610174ce2977ee1cf008a8861e9304868a6093
vominhtam1704 <160805542+vominhtam1704@users.noreply.github.com>
2024-02-29 11:11:54 +0700

    Calculation.java

---
+++ Calculation.java
@@ -0,0 +1,39 @@
+package src;
+public class Calculation {
+    private int a;
+    private int b;
+    
+    public Calculation(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int sum() {
+        return a + b;
+    }
+
+    public int minus() {
+        return a - b;
+    }
+
+    public int mul() {
+        return a * b;
+    }
+
+    public float div() {
+        return (float) a / b;
+    }
+}
+package src;
+
+public class main {
+    public static void main(String[] args) {
+//        Calculation cal1 = new Calculation(5, 7);
+//        System.out.println(cal1.sum());
+//        System.out.println( cal1.minus());
+//        System.out.println(cal1.mul());
+//        System.out.println(cal1.div());
+//        System.out.println("stop!");
+
+    }
+}
