f31265829b4e71a23591b7aec362d78e3bab8738
vominhtam1704 <160805542+vominhtam1704@users.noreply.github.com>
2024-02-29 11:07:48 +0700

    Car.java

---
+++ Car.java
@@ -0,0 +1,20 @@
+package myPackage;
+
+public class Cars {
+        int id;
+        String name;
+
+        public Cars(int id, String name) {
+                this.id = id;
+                this.name = name;
+        }
+
+        public static void main(String[] args) {
+                Cars car1 = new Cars(1, "SH");
+                Cars car2 = new Cars(2, "Vison");
+                Cars car3 = new Cars(3, "Honda");
+        }
+}
+        }
+    }
+}
